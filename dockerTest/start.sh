9e46f2e7bf7671b290eb06a70db0cde561b8c424
blahblah <leesemin222@gmail.com>
2024-10-14 19:04:22 +0900

    저장

---
+++ dockerTest/start.sh
@@ -0,0 +1,14 @@
+# 환경변수 설정
+export AccessToken=""
+
+# 도커 버전 확인
+docker --version
+
+# 호스트의 Docker에 접근하여 컨테이너 목록 출력
+docker ps
+
+# 로그 메시지 출력
+echo "DooD enviroment is ready!"
+
+# 컨테이너가 계속 실행되도록 유지
+tail -f /dev/null
